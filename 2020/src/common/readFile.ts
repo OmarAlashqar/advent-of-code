016a8201b0b06047a05c03144c1fd7573d72fe93
Omar Alashqar <o.ashqar@outlook.com>
2020-12-09 18:28:24 -0500

    refactor readFIle to allow for other file inputs

--- 2020/src/common/readFile.ts
+++ 2020/src/common/readFile.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
+import path from 'path';
 
-export default async (directory = '.'): Promise<string> => {
+export default async (directory = '.', filename = 'input.txt'): Promise<string> => {
   return new Promise((resolve, reject) => {
-    fs.readFile(`${directory}/input.txt`, 'utf8', (err, data) => {
+    fs.readFile(path.join(directory, filename), 'utf8', (err, data) => {
       if (err) reject(err);
       else resolve(data);
     });
