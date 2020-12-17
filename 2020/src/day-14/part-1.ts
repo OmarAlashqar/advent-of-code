4a83e5dde36ed03b92902f9a7e5793f429475e60
Omar Alashqar <o.ashqar@outlook.com>
2020-12-16 19:52:04 -0500

    day14 part 1

---
+++ 2020/src/day-14/part-1.ts
@@ -0,0 +1,50 @@
+// answer: 17765746710228
+import readFile from '../common/readFile';
+
+// ayy bit manipulation, my jam! hype!
+
+// after questioning life, I stumbled upon this piece from the js documentation:
+// "JavaScript stores numbers as 64 bits floating point numbers, but all bitwise operations are performed on 32 bits binary numbers."
+
+// this means we can't use the built-in bitwise operations on our 36-bit masks and values :(
+// nevertheless, the BigInt type implements bitwise operations for wider numbers! woo!
+
+const ADDRESS_RANGE = 2 ^ 36;
+const BIG_0 = BigInt(0);
+
+export const solution = (input: string[]): number => {
+  const mem = new Array<bigint>(ADDRESS_RANGE).fill(BIG_0);
+  let coldMask = BigInt(0x000000000); // has 'X's replaced with '0's
+  let hotMask = BigInt(0xfffffffff); // has 'X's replaced with '1's
+
+  for (let i = 0; i < input.length; i++) {
+    const line = input[i];
+
+    if (line[1] === 'a') {
+      // mask command
+      const match = line.match(/mask = ([X01]+)/);
+      if (!match) throw new Error(`Encountered faulty command: ${line}`);
+
+      const mask = match[1];
+      coldMask = BigInt(parseInt(mask.replace(/X/g, '0'), 2));
+      hotMask = BigInt(parseInt(mask.replace(/X/g, '1'), 2));
+    } else if (line[1] === 'e') {
+      // mem command
+      const match = line.match(/mem\[(\d+)\] = (\d+)/);
+      if (!match) throw new Error(`Encountered faulty command: ${line}`);
+
+      const addr = Number(match[1]);
+      const value = BigInt(match[2]);
+
+      // the real magic happens here, behold!
+      mem[addr] = (value | coldMask) & hotMask;
+    }
+  }
+
+  return Number(mem.reduce((acc, v) => acc + v, BIG_0));
+};
+
+if (require.main === module) {
+  const raw = readFile(__dirname).split('\n');
+  console.log(solution(raw));
+}
